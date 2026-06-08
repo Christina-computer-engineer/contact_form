3a34eb3a4b6246c0859ad6911ed6ead3e4922a25
Christina-computer-engineer <togetchristinaraphel@gmail.com>
2026-06-08 20:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    let nameError = document.getElementById("nameError");
+    let emailError = document.getElementById("emailError");
+    let messageError = document.getElementById("messageError");
+    let successMessage = document.getElementById("successMessage");
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+    successMessage.textContent = "";
+
+    let isValid = true;
+
+    let namePattern = /^[A-Za-z ]+$/;
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(name === ""){
+        nameError.textContent = "Name is required";
+        isValid = false;
+    }
+    else if(!namePattern.test(name)){
+        nameError.textContent = "Name should contain only letters";
+        isValid = false;
+    }
+
+    if(email === ""){
+        emailError.textContent = "Email is required";
+        isValid = false;
+    }
+    else if(!emailPattern.test(email)){
+        emailError.textContent = "Enter a valid email address";
+        isValid = false;
+    }
+
+    if(message === ""){
+        messageError.textContent = "Message is required";
+        isValid = false;
+    }
+
+    if(isValid){
+        successMessage.textContent = "✅ Form submitted successfully!";
+        document.getElementById("contactForm").reset();
+    }
+
+});
